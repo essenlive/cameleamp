bfaaf4df99db91f87ff49b010f91491243e14991
quentin <perchais.quentin@gmail.com>
2021-06-29 18:09:29 +0200

    ⚡Added PCB and scematic files

--- cameleamp.ino
+++ cameleamp.ino
@@ -1,16 +1,16 @@
-const int RED = 11;
-const int GREEN = 10;
+const int RED = 10;
+const int GREEN = 11;
 const int BLUE = 9;
-const int S0 = 2;
-const int S1 = 3;
-const int S2 = 7;
-const int S3 = 8;
+const int S0 = 7;
+const int S1 = 8;
+const int S2 = 3;
+const int S3 = 2;
 const int sensorOut = 6;
 //////////////////////////////////
 
 // Stores frequency read by the photodiodes
 int redFrequency = 0;
-int greenFrequency = 0;
+int greenFrequency = 0; 
 int blueFrequency = 0;
 
 int redColor = 0;
@@ -20,16 +20,16 @@ int blueColor = 0;
 void setup()
 {
   // Setting the outputs
-  pinMode(S0, OUTPUT);
-  pinMode(S1, OUTPUT);
-  pinMode(S2, OUTPUT);
-  pinMode(S3, OUTPUT);
+  // pinMode(S0, OUTPUT);
+  // pinMode(S1, OUTPUT);
+  // pinMode(S2, OUTPUT);
+  // pinMode(S3, OUTPUT);
   // Setting the sensorOut as an input
-  pinMode(sensorOut, INPUT);
+  // pinMode(sensorOut, INPUT);
 
   // Setting frequency scaling to 20%
-  digitalWrite(S0, HIGH);
-  digitalWrite(S1, LOW);
+  // digitalWrite(S0, HIGH);
+  // digitalWrite(S1, LOW);
 
   //LED Setup
   pinMode(RED, OUTPUT);
@@ -47,40 +47,45 @@ void setup()
 void loop()
 {
   // Setting RED (R) filtered photodiodes to be read
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, LOW);
+  // digitalWrite(S2, LOW);
+  // digitalWrite(S3, LOW);
   // Reading the output frequency
-  redFrequency = pulseIn(sensorOut, LOW);
-  delay(100);
+  // redFrequency = pulseIn(sensorOut, LOW);
+  // delay(100);
   // Setting GREEN (G) filtered photodiodes to be read
-  digitalWrite(S2, HIGH);
-  digitalWrite(S3, HIGH);
-  greenFrequency = pulseIn(sensorOut, LOW);
-  delay(100);
+  // digitalWrite(S2, HIGH);
+  // digitalWrite(S3, HIGH);
+  // greenFrequency = pulseIn(sensorOut, LOW);
+  // delay(100);
   // Setting BLUE (B) filtered photodiodes to be read
-  digitalWrite(S2, LOW);
-  digitalWrite(S3, HIGH);
-  blueFrequency = pulseIn(sensorOut, LOW);
-  delay(100);
+  // digitalWrite(S2, LOW);
+  // digitalWrite(S3, HIGH);
+  // blueFrequency = pulseIn(sensorOut, LOW);
+  // delay(100);
 
   // Printing the RED (R) value
-  Serial.print("R = ");
-  Serial.print(redFrequency);
+  // Serial.print("R = ");
+  // Serial.print(redFrequency);
   // Printing the GREEN (G) value
-  Serial.print(" G = ");
-  Serial.print(greenFrequency);
+  // Serial.print(" G = ");
+  // Serial.print(greenFrequency);
   // Printing the BLUE (B) value
-  Serial.print(" B = ");
-  Serial.println(blueFrequency);
+  // Serial.print(" B = ");
+  // Serial.println(blueFrequency);
 
-  redColor = map(redFrequency, 60, 281, 255, 0);
-  greenColor = map(greenFrequency, 70, 412, 255, 0);
-  blueColor = map(blueFrequency, 70, 164, 255, 0);
+  // redColor = map(redFrequency, 60, 281, 255, 0);
+  // greenColor = map(greenFrequency, 70, 412, 255, 0);
+  // blueColor = map(blueFrequency, 70, 164, 255, 0);
 
-  analogWrite(RED, redColor);
-  analogWrite(GREEN, greenColor);
-  analogWrite(BLUE, blueColor);
-  delay(1000);
+  // analogWrite(RED, redColor);
+  // analogWrite(GREEN, greenColor);
+  // analogWrite(BLUE, blueColor);
+  // delay(1000);
+
+  analogWrite(RED, 0);
+  analogWrite(GREEN, 0);
+  analogWrite(BLUE, 255);
+
   // for (size_t i = 0; i < 255; i++)
   // {
   //   analogWrite(RED, i);
